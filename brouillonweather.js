7af2bab939697e2d819985c723a9017bb3ff8a89
Alicia Mendez Cruz <aliciamendezcruz@alichus-mbp.home>
2024-01-03 17:28:21 +0100

    frustration

---
+++ brouillonweather.js
@@ -0,0 +1,39 @@
+const rootRefToday = document.getElementById("rootToday");
+const todayDateRootRef = document.getElementById("todayDateRoot");
+const weatherIconRootRef = document.getElementById("weatherIconRoot");
+//const todayTempRootRef = document.getElementById("todayTempRoot");
+const todayTempMaxRootRef = document.getElementById("todayTempMaxRoot");
+const todayTempMinRootRef = document.getElementById("todayTempMinRoot");
+const todayDescriptionRootRef = document.getElementById("todayDescriptionRoot");
+const todayCityRootRef = document.getElementById("todayCityRoot");
+
+const citySearchInputRef = document.getElementById("citySearchInput");
+
+const spinner = `<div class="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>`;
+
+todayDateRootRef.innerHTML = `TODAY`;
+//todayTempRootRef.innerHTML = `here goes temp`;
+todayTempMaxRootRef.innerHTML = `here goes the max temp`;
+todayTempMinRootRef.innerHTML = `here goes the min temp`;
+todayDescriptionRootRef.innerHTML = `here goes the description`;
+todayCityRootRef.innerHTML = `here goes city Name`;
+
+async function getAPIData(cityname) {
+  const result = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&&units=metric&appid=e135612bb4f8e46add41bdebe1c186ab`
+  );
+  console.log(result.data);
+
+  const { name, temp, max_temp, min_temp, description, dt } = result.data[0];
+
+  todayDateRootRef.innerHTML = `${dt}`;
+  todayTempRootRef.innerHTML = `${temp}`;
+  todayTempMaxRootRef.innerHTML = `${max_temp}`;
+  todayTempMinRootRef.innerHTML = `${min_temp}`;
+  todayDescriptionRootRef.innerHTML = `${description}`;
+  todayCityRootRef.innerHTML = `${name}`;
+}
+
+citySearchInputRef.addEventListener("input", (e) => {
+  getAPIData(e.target.cityname);
+});
